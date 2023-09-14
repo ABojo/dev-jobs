ac4a96f2f2e247ecea148c48cc269745443aded0
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-13 22:10:21 -0400

    Added routing and getListingById function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import JobGrid from "./components/JobGrid/JobGrid";
+import JobListing from "./components/JobListing/JobListing";
+import JobListingT from "./types/JobListing";
+import jobListings from "./utils/data.json";
+
 function App() {
-  return <div></div>;
+  function getJobListingById(id: number): JobListingT | null {
+    return jobListings.find((job) => job.id === id) || null;
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<JobGrid jobListings={jobListings.slice(0, 12)} />} />
+            <Route path="/listing/:id" element={<JobListing getJobListingById={getJobListingById} />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+  );
 }
 
 export default App;
