453dc05579e3804ab7bcd1647c5b94cbffe675a9
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-13 20:36:17 -0400

    Added theme toggle logic and styling the header
    I used a checkbox and label to build a custom toggler for the theme. It uses the
    isDark state to determine its checked state, and it uses the checked psuedo
    class to move the toggle's dot according to the current state.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,19 @@
 import styles from "./Header.module.scss";
+import useTheme from "../../hooks/useTheme";
 
 function Header() {
+  const [isDark, toggleTheme] = useTheme();
+
   return (
     <header className={styles.header}>
       <div className={styles.header__inner}>
         <h1 className={styles.header__heading}>devjobs</h1>
-        <button>Placeholder</button>
+        <div className={styles.header__theme}>
+          <input className={styles.header__check} id="theme" type="checkbox" checked={isDark} onChange={toggleTheme} />
+          <img src="/images/desktop/icon-sun.svg" alt="" />
+          <label htmlFor="theme"></label>
+          <img src="/images/desktop/icon-moon.svg" alt="" />
+        </div>
       </div>
     </header>
   );
