3c15518043a24f04c7c7d0853cb7693a7961146d
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-15 11:46:17 -0400

    Created the FilterForm component
    Built out the filter form so users will be able to filter the jobs that are
    listed in the grid.

---
+++ src/components/FilterForm/FilterForm.tsx
@@ -0,0 +1,64 @@
+import FormState from "../../types/FormState";
+import FormAction from "../../types/FormAction";
+import styles from "./FilterForm.module.scss";
+
+interface FilterFormProps {
+  formState: FormState;
+  formDispatch: React.Dispatch<FormAction>;
+  runFilter: () => void;
+}
+
+function FilterForm({ formState, formDispatch, runFilter }: FilterFormProps) {
+  return (
+    <form
+      className={styles.form}
+      onSubmit={(e) => {
+        e.preventDefault();
+        runFilter();
+      }}
+    >
+      <div className={`${styles.form__input} ${styles["form__input--title"]}`}>
+        <img className={styles.form__icon} src="/images/desktop/icon-search.svg" alt="" />
+        <input
+          onChange={(e) => {
+            formDispatch({ type: "SET_TITLE", payload: e.target.value });
+          }}
+          value={formState.title}
+          type="text"
+          placeholder="Filter by title..."
+          aria-label="Job title"
+        />
+      </div>
+      <div className={`${styles.form__input} ${styles["form__input--location"]}`}>
+        <img className={styles.form__icon} src="/images/desktop/icon-location.svg" alt="" />
+        <input
+          onChange={(e) => {
+            formDispatch({ type: "SET_LOCATION", payload: e.target.value });
+          }}
+          value={formState.location}
+          type="text"
+          placeholder="Filter by location..."
+          aria-label="Location"
+        />
+      </div>
+      <div className={styles.form__third}>
+        <div className={styles.form__full}>
+          <input
+            onChange={(e) => {
+              formDispatch({ type: "SET_FULL_TIME", payload: e.target.checked });
+            }}
+            checked={formState.fullTime}
+            id="full-time"
+            className={styles.form__check}
+            type="checkbox"
+          />
+          <label htmlFor="full-time"></label>
+          <p>Full-Time Only</p>
+        </div>
+        <button className={styles.form__submit}>Search</button>
+      </div>
+    </form>
+  );
+}
+
+export default FilterForm;
