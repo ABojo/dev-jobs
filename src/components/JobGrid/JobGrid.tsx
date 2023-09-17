b25b6b0947a99dd65cc9e8eb8712b49235823f6a
ABojo <Alex.bojorquez2598@gmail.com>
2023-09-16 23:41:50 -0400

    Added a results count to the UI

--- src/components/JobGrid/JobGrid.tsx
+++ src/components/JobGrid/JobGrid.tsx
@@ -9,6 +9,7 @@ interface JobGridProps {
 function JobGrid({ jobListings }: JobGridProps) {
   return (
     <div className={styles.container}>
+      <p className={styles.results}>Job Listings ({jobListings.length})</p>
       <div className={styles.grid}>
         {jobListings.map((job) => {
           return <JobCard key={job.id} jobListing={job} />;
